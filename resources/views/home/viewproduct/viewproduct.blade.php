cf3a2dbf63a6c5f8fb97f76170a87f31e2107951
huybao20022 <huybaoshaw3@gmail.com>
2022-08-24 00:29:41 +0700

    add viewproduct

--- resources/views/home/viewproduct/viewproduct.blade.php
+++ resources/views/home/viewproduct/viewproduct.blade.php
@@ -26,10 +26,220 @@
     </div>
     <hr>
     {{-- carousel --}}
-    <div class="container">
+    <div class="container mt-5 mb-5" id="productPage">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card">
+                    <img src="{{asset('img/'. $data->product_image)}}" class="figure-img img-fluid rounded">
 
+                </div>
+            </div>
+
+            <div class="col-md-7">
+                <h5>{{$data->product_title}}</h5>
+                <p class="text-muted">{{$data->product_cat}}</p>
+                <h5 class="pt-4">${{$data->product_price}}</h5>
+                <p class="description text-muted">{{$data->product_details}}</p>
+                <hr>
+                <table class="table mt-2">
+
+                    <tbody>
+                        <tr>
+                            <th scope="row">Color</th>
+                            <td>Black</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Delivery</th>
+                            <td>Bangalore, KA, India - 36</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="input-group">
+                            <div class="input-group-prepend" onclick="add1()">
+                                <span class="input-group-text"><i class="fas fa-plus"></i></span>
+                            </div>
+
+                            <input type="text" class="numberbox1 form-control" value="0">
+                            <div class="input-group-append" onclick="subtract1()">
+                                <span class="input-group-text"><i class="fas fa-minus"></i></span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div><!-- row -->
+                <div id="purchase-btn">
+                    <button type="button" class="btn btn-sm btn-primary"><a class="text-white" href=""> Buy Now
+                        </a></button>
+                    &nbsp;
+                    <button type="button" class="btn btn-sm btn-light"><a class="text-black" href=""> <i class="fas fa-shopping-cart pr-2"></i> Add to Cart </a> </button>
+                </div>
+            </div>
+            <div class="container">
+                <ul class="nav nav-tabs mt-5" id="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">DESCRIPTION</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">INFORMATION</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">REVIEWS (1)</a>
+                    </li>
+                </ul>
+                <div class="tab-content w-100 pt-5" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <h5>Product Description</h5>
+                        <p class="text-muted">T-SHIRTS</p>
+                        <div class="ratings">
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="far fa-star text-primary"></i>
+
+                        </div>
+                        <h5 class="pt-4">$179.00</h5>
+                        <p class="description text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                            anim id est laborum.
+
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <h5>Additional Information</h5>
+                        <br>
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="row">Laptop Gaming MSI Bravo 15 B5DD 276VN</th>
+                                    <td><i></i> </td>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                <tr>
+                                    <th scope="row">CPU</th>
+                                    <td><i>AMD Ryzen 5-5600H 3.30GHz upto 4.20GHz, 6 cores 12 threads</i></td>
+                                </tr>
+
+                            </tbody>
+
+                            <tbody>
+
+                                <tr>
+                                    <th scope="row">RAM</th>
+                                    <td><i>DDR4 8GB (1 x 8GB) 3200MHz; 2 slots, Up to 64GB</i></td>
+                                </tr>
+
+                            </tbody>
+
+                            <tbody>
+
+                                <tr>
+                                    <th scope="row">Hard drive</th>
+                                    <td><i>512GB NVMe PCIe Gen3x4 SSD</i></td>
+                                </tr>
+
+                            </tbody>
+
+                            <tbody>
+
+                                <tr>
+                                    <th scope="row">Card</th>
+                                    <td><i>Radeon RX5500M 4GB</i></td>
+                                </tr>
+
+                            </tbody>
+
+                            <tbody>
+
+                                <tr>
+                                    <th scope="row">Monitor</th>
+                                    <td><i>R15.6 inch FHD (1920*1080), 60Hz 45%NTSC IPS-Level</i></td>
+                                </tr>
+
+                            </tbody>
+
+
+                        </table>
+                    </div>
+                    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                        <div class="review-rating float-right mt-5">
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="far fa-star text-primary"></i>
+
+                        </div>
+                        <h5>1 review for Fantasy T-shirt</h5>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-6 mt-2">
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <img src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="IMAGE LOADING" class="img-fluid">
+                                    </div>
+                                    <div class="col-md-10">
+                                        <p class="mb-1"><strong> Marthasteward </strong> – Jan 28, 2020 </p>
+                                        <p>Nice one, love it!</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <hr>
+
+                        <h5 class="mt-3 pt-2">Add a Review</h5>
+                        <p>Your email address will not be published.</p>
+                        <div class="ratings">
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="fas fa-star text-primary"></i>
+                            <i class="far fa-star text-primary"></i>
+
+                        </div>
+                        <form class="mt-3" id="addreview">
+                            <div class="form-group text-muted">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" placeholder="Your Review"></textarea>
+                            </div>
+                            <div class="form-group text-muted">
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group text-muted">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                            </div>
+
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">Add a Review</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+                <br><br>
+                <hr>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
     </div>
-    <a>{{ $data->producer_name }}</a>
     <br>
     <br>
     {{-- footer --}}
